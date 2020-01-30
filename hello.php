fe23f167c6261edf25877197c4c948bc54f88aaf
yoppinbass <yoppinbass@gmail.com>
2020-01-30 17:22:19 +0900

    Add hello.php

---
+++ hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>PHP Basic</title>
+</head>
+<body>
+<?
+// comment
+print 'Hello World <br />';
+print 'Hello Everyone';
+?>
+</body>
+</html>
