113226e70369614ef697929d7ea167972bc080e4
yoppi <yoppinbass@gmail.com>
2020-02-07 15:35:39 +0900

    Adding second csv file

--- mycode/csv.php
+++ mycode/csv.php
@@ -1,22 +1,32 @@
 <?php
 
-$file = fopen('hoge.csv', 'rb');
-flock($file, LOCK_SH);
-
-$data = [];
-while ($line = fgetcsv($file, 1024, ',')) {
-    $item = $line;
-    $data[] = $item;
-}
-
 function show($data) {
     foreach ($data as $item) {
-        echo "\n";
         foreach ($item as $i) {
             echo $i . "\t";
         }
+        echo "\n";
     }
 }
+
+function readcsv($file_name) {
+    // ファイルの読み込み
+    $file = fopen($file_name, 'rb');
+    flock($file, LOCK_SH);
+
+    // $data にcsvを格納
+    $data = [];
+    while ($line = fgetcsv($file, 1024, ',')) {
+        $item = $line;
+        $data[] = $item;
+    }
+
+    // ファイルを閉じる
+    flock($file, LOCK_UN);
+    fclose($file);
+    return $data;
+}
+$data=readcsv('hoge.csv');
+$config=readcsv('config.csv');
 show($data);
-flock($file, LOCK_UN);
-fclose($file);
+show($config);
